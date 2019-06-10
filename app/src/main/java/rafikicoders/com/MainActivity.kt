537d9f74bf991be1f6300704bed0d1a0da40274f
modongo <mikodongo@gmail.com>
2019-06-10 11:44:01 +0300

    Initial commit

---
+++ app/src/main/java/rafikicoders/com/MainActivity.kt
@@ -0,0 +1,12 @@
+package rafikicoders.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
